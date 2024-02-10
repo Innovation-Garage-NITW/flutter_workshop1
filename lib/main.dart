f3ab78a339fc754bc16e105997aae45875ba1734
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-10 10:17:55 +0530

    widget3

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,21 @@ void main() {
   runApp(
     MaterialApp(
       title: "Flutter Workshop",
-      home: Scaffold(appBar: AppBar(), body: Text('Flutter Workshop')),
+      // theme: ThemeData(fontFamily: 'Roboto'),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text(
+            'Inspiration',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
