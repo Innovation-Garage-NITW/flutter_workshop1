9962385dbe1947cd9eefa59b1f20b5a59ce8d7af
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-10 09:58:56 +0530

    widget2

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       title: "Flutter Workshop",
-      home: Scaffold(
-        appBar: AppBar(),
-        body: const Center(
-          child: Text(
-            'Inspiration',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(appBar: AppBar(), body: Text('Flutter Workshop')),
     ),
   );
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [Wiget1(), ..., Widgetn()],
-    );
-  }
 }
